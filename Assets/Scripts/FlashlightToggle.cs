5ad3f4e4b3672f8ba8444c358d7042bc5ae09d5c
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-14 20:49:42 -0400

    Light flicker

--- Assets/Scripts/FlashlightToggle.cs
+++ Assets/Scripts/FlashlightToggle.cs
@@ -1,14 +1,37 @@
 using UnityEngine;
+using System.Collections;
 
 public class FlashlightToggle : MonoBehaviour
 {
     public Light flashlight;
+    private bool isOn = false;
+    private bool isFlickering = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T) && !isFlickering)
         {
-            flashlight.enabled = !flashlight.enabled;
+            isOn = !isOn;
+
+            if (isOn)
+                StartCoroutine(FlickerOn());
+            else
+                flashlight.enabled = false;
         }
+    }
+
+    IEnumerator FlickerOn()
+    {
+        isFlickering = true;
+
+        for (int i = 0; i < 3; i++)
+        {
+            flashlight.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+            flashlight.enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+        }
+
+        isFlickering = false;
     }
 }
