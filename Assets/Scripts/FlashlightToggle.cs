e104e8387db8c085d689b16cb7d0fdb8b896699e
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-16 01:58:34 -0400

    Flashlight ui icon

--- Assets/Scripts/FlashlightToggle.cs
+++ Assets/Scripts/FlashlightToggle.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class FlashlightToggle : MonoBehaviour
 {
     public Light flashlight;
+    public Image flashlightIcon;
+
     private bool isOn = false;
     private bool isFlickering = false;
 
@@ -17,6 +20,8 @@ public class FlashlightToggle : MonoBehaviour
                 StartCoroutine(FlickerOn());
             else
                 flashlight.enabled = false;
+
+            UpdateIcon();
         }
     }
 
@@ -33,5 +38,15 @@ public class FlashlightToggle : MonoBehaviour
         }
 
         isFlickering = false;
+    }
+
+    void UpdateIcon()
+    {
+        if (flashlightIcon != null)
+        {
+            flashlightIcon.color = isOn
+                ? Color.white
+                : new Color(0.5f, 0.5f, 0.5f, 0.4f); // gray and slightly transparent when off
+        }
     }
 }
